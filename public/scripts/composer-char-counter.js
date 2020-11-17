f1b6c7e9b75654f032055de73b61e3f11bd7f066
Jared <jaredflomen@gmail.com>
2020-11-17 18:56:59 +0000

    Adding CSS to change the character count red if > 140 characters

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
   $('#tweet-text').keyup(function () {
     const tweetSpot = $(this)
-    const locateCounter = tweetSpot.parent().children('#below-txt-box').children('.counter');
+    const locateCounter = tweetSpot.parent().children('#below-txt-box').children('.counter')
+
     locateCounter.text(140 - tweetSpot.val().length)
+    
+    if (tweetSpot.val().length > 140) {
+      locateCounter.css('color', 'red');
+    } else {
+      locateCounter.css('color', 'black')
+    }
+
   });
 });
