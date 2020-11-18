6853c20369537070cecdb9f88e38c33e1ee3a615
Jared <jaredflomen@gmail.com>
2020-11-18 19:31:06 +0000

    Adding AJAX get request to receive object containing tweet information

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,32 @@
  */
 $(document).ready(() => {
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
+
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 const renderTweets = function(tweets) {
   //loops through the tweets
@@ -42,7 +44,7 @@ const renderTweets = function(tweets) {
 
 const createTweetElement = function(tweet) {
   //Generates a DOM structure for a tweet
-  let $tweet = $(`
+  let $tweet2 = $(`
   <article class="tweet">
     <header>
       <img src="${tweet.user.avatars}" height="40"> 
@@ -59,10 +61,23 @@ const createTweetElement = function(tweet) {
       <i class="fas fa-heart"></i>
     </footer>
   </article>`);
-  return $tweet;
+  return $tweet2;
 }
 
-renderTweets(data);
+const loadTweets = () => {
+     $.ajax({
+      url: '/tweets',
+      method: 'GET',
+    })
+    .then(res => {
+      console.log("worked", res);
+      renderTweets(res);
+    })
+}
+
+loadTweets();
+
+// renderTweets(data);
 
   $('form').on('submit', event => {
     event.preventDefault()
@@ -71,6 +86,10 @@ renderTweets(data);
       method: 'POST',
       data: $('form').serialize()
     })
-    .then(res => console.log(res))
+    .then(res => {
+      console.log($('form').serialize())
+      $('.tweet-target').empty()
+      loadTweets();
+    })
   })
 })
